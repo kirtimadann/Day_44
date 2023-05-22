3ea41adda76494401bc1f7ebe7aa63edeb83159c
lunainfotec <kirtimadan1997@gmail.com>
2023-05-22 17:14:47 +0530

    added : UC2

--- employeePayRoll.js
+++ employeePayRoll.js
@@ -3,14 +3,17 @@ class EmployeePayrollData {
     set id(id){
       this._id = id; 
     }
+
+    
     
     get name() { return this._name; } 
     set name(name) {
-    let nameRegex = RegExp('^[A-Z]{1} [a-zA-Z\\s]{2,}$')
-    if (nameRegex.test(name))
-       this._name = name;
-    else throw 'Name is Incorrect!';
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$')
+        if (nameRegex.test (name)) 
+        this._name = name;
+       else throw 'Name Incorrect';
     }
+    
     
     get profilePic() { return this._profilePic; } 
     set profilePic(profilePic) {
@@ -61,3 +64,4 @@ toString() {
 
 }
 }
+
--- new_EmployeePayRoll.js
+++ new_EmployeePayRoll.js
@@ -1,3 +1,22 @@
+
+window.addEventListener('DOMContentLoaded', (event) => { 
+     const name = document.querySelector('#name'); 
+     const textError = document.querySelector('.text-error');
+     name.addEventListener('input', function() {
+      if (name.value.length == 0) {
+     textError.textContent = "";
+     return;
+     }
+     try {
+     (new EmployeePayrollData()).name = name.value;
+     textError.textContent = " ";
+     } catch (e) {
+     textError.textContent = e;
+     }
+     });
+     });
+ 
+
 const salary = document.querySelector('#salary'); 
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
@@ -6,21 +25,5 @@ salary.addEventListener('input', function()
      output.textContent = salary.value;
 });
 
-window.addEventListener('DOMContentLoaded', (event) => { 
-    const name = document.querySelector('#name'); 
-    const textError = document.querySelector('.text-error');
-    name.addEventListener('input', function() {
-     if (name.value.length == 0) {
-    textError.textContent = "";
-    return;
-    }
-    try {
-    (new EmployeePayRoll()).name = name.value;
-    textError.textContent = " ";
-    } catch (e) {
-    textError.textContent = e;
-    }
-    });
-    });
 
 
