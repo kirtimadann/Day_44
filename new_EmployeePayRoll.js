562fa930681d13968e2191347636315fe8d7f620
lunainfotec <kirtimadan1997@gmail.com>
2023-05-22 16:50:29 +0530

    added : UC1

---
+++ new_EmployeePayRoll.js
@@ -0,0 +1,26 @@
+const salary = document.querySelector('#salary'); 
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function() 
+{
+     output.textContent = salary.value;
+});
+
+window.addEventListener('DOMContentLoaded', (event) => { 
+    const name = document.querySelector('#name'); 
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+     if (name.value.length == 0) {
+    textError.textContent = "";
+    return;
+    }
+    try {
+    (new EmployeePayRoll()).name = name.value;
+    textError.textContent = " ";
+    } catch (e) {
+    textError.textContent = e;
+    }
+    });
+    });
+
+
